5152f5a121365a0585dd4ef8a220f8560879e153
Sadler2 <sad2004@mail.ru>
2019-07-20 17:33:00 +0600

    Russian locale (fixed)

---
+++ locales/ru.lua
@@ -0,0 +1,6 @@
+Locales['ru'] = {
+  ['skin_menu'] = 'Меню скинов',
+  ['use_rotate_view'] = 'используйте ~INPUT_VEH_FLY_ROLL_LEFT_ONLY~ и ~INPUT_VEH_FLY_ROLL_RIGHT_ONLY~ для вращения камеры.',
+  ['skin'] = 'изменить скин',
+  ['saveskin'] = 'сохранить скин в файл',
+}
