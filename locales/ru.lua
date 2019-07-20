cfea448c430b70242250e62f502a3a5d0cdef356
Sadler2 <sad2004@mail.ru>
2019-07-20 17:36:19 +0600

    Skin Menu control camera angle and zoom with mouse

--- locales/ru.lua
+++ locales/ru.lua
@@ -1,6 +1,6 @@
 Locales['ru'] = {
   ['skin_menu'] = 'Меню скинов',
-  ['use_rotate_view'] = 'используйте ~INPUT_VEH_FLY_ROLL_LEFT_ONLY~ и ~INPUT_VEH_FLY_ROLL_RIGHT_ONLY~ для вращения камеры.',
+  ['use_rotate_view'] = 'используйте мышь для вращения и масштаба камеры.',
   ['skin'] = 'изменить скин',
   ['saveskin'] = 'сохранить скин в файл',
 }
